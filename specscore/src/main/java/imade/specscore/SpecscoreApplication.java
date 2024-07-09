3118594b185e1595eabfc3594956964b34fe025e
kimseunggi <ninny9988@naver.com>
2024-07-09 17:54:33 +0900

    first commit

---
+++ specscore/src/main/java/imade/specscore/SpecscoreApplication.java
@@ -0,0 +1,13 @@
+package imade.specscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecscoreApplication.class, args);
+	}
+
+}
---
+++ specscore/src/test/java/imade/specscore/SpecscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package imade.specscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
