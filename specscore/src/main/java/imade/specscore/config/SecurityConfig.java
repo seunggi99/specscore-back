6f3806ab5c48ad46787c2be80ce1f481dc2ddadc
kimseunggi <ninny9988@naver.com>
2024-07-10 10:35:51 +0900

    security 로그인 없이 페이지 접근 가능하게 수정

---
+++ specscore/src/main/java/imade/specscore/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package imade.specscore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // 모든 요청 허용
+                )
+                .csrf(withDefaults()); // CSRF 보호 활성화
+
+        return http.build();
+    }
+
+}
