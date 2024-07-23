81fcb2d5ac89e9d6d26013648a918ddc61e96461
Dev-SSW <sgg919@naver.com>
2024-07-23 16:45:57 +0900

    Fix: User의 id를 다시 Id로 바꿈

--- specscore/src/main/java/imade/specscore/domain/User.java
+++ specscore/src/main/java/imade/specscore/domain/User.java
@@ -21,7 +21,7 @@ public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long id;
+    private Long Id;
 
     private String username;
 
